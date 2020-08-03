ebb9b29dfea86d3f8ab236009ac9509aa0b689f3
epictete <john.laterre@gmail.com>
2020-08-03 22:01:50 +0200

    Added dead face and a few minor updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,157 +5,159 @@ const word = document.getElementById("word");
 const alphabet = document.getElementById("alphabet");
 const delay = 100;
 const words = [
-    'ability',
-    'inspector',
-    'affair',
-    'candidate',
-    'funeral',
-    'solution',
-    'youth',
-    'mom',
-    'nature',
-    'child',
-    'chest',
-    'year',
-    'topic',
-    'phone',
-    'pollution',
-    'basis',
-    'artisan',
-    'promotion',
-    'depression',
-    'situation',
-    'classroom',
-    'failure',
-    'tradition',
-    'foundation',
-    'intention',
-    'competition',
-    'user',
-    'proposal',
-    'preparation',
-    'combination',
-    'reaction',
-    'movie',
-    'attitude',
-    'friendship',
-    'passenger',
-    'lab',
-    'marketing',
-    'bathroom',
-    'uncle',
-    'story',
-    'championship',
-    'throat',
-    'sister',
-    'quantity',
-    'tea',
-    'birthday',
-    'news',
-    'version',
-    'advice',
-    'mall'
+  "ability",
+  "inspector",
+  "affair",
+  "candidate",
+  "funeral",
+  "solution",
+  "youth",
+  "mom",
+  "nature",
+  "child",
+  "chest",
+  "year",
+  "topic",
+  "phone",
+  "pollution",
+  "basis",
+  "artisan",
+  "promotion",
+  "depression",
+  "situation",
+  "classroom",
+  "failure",
+  "tradition",
+  "foundation",
+  "intention",
+  "competition",
+  "user",
+  "proposal",
+  "preparation",
+  "combination",
+  "reaction",
+  "movie",
+  "attitude",
+  "friendship",
+  "passenger",
+  "lab",
+  "marketing",
+  "bathroom",
+  "uncle",
+  "story",
+  "championship",
+  "throat",
+  "sister",
+  "quantity",
+  "tea",
+  "birthday",
+  "news",
+  "version",
+  "advice",
+  "mall",
 ];
 
 //Variables
 let game = {
-    score: 0,
-    tries: 6,
-    fails: 0,
-    indexes: [],
-    target: ''
-}
+  score: 0,
+  tries: 6,
+  fails: 0,
+  indexes: [],
+  target: "",
+};
 
 //Event listeners
 start.onclick = () => {
-    hangMan();
-    start.disabled = true;
+  hangMan();
+  start.disabled = true;
 };
-reset.onclick = () => {
-    document.location.reload(true);
-};
+reset.onclick = () => document.location.reload(true);
 alphabet.onclick = checkLetter;
 
 //Functions
 function checkLetter(e) {
-    const letter = e.target.innerHTML.toLocaleLowerCase();
-    console.log(letter);
-    if (letter.length === 1) {
-        if (game.target.includes(letter)) {
-            matchCount(letter);
-            guessUpdate(letter);
-            e.target.style.backgroundColor = 'green';
-            e.target.disabled = true;
-            window.setTimeout(win, delay);
-        } else {
-            game.fails++;
-            draw();
-            e.target.style.backgroundColor = 'red';
-            e.target.disabled = true;
-            window.setTimeout(gameOver, delay);
-        }
+  const letter = e.target.innerHTML.toLocaleLowerCase();
+  console.log(letter);
+  if (letter.length === 1) {
+    if (game.target.includes(letter)) {
+      matchCount(letter);
+      guessUpdate(letter);
+      e.target.style.backgroundColor = "green";
+      e.target.disabled = true;
+      window.setTimeout(win, delay);
+    } else {
+      game.fails++;
+      draw();
+      e.target.style.backgroundColor = "red";
+      e.target.disabled = true;
+      window.setTimeout(gameOver, delay);
     }
+  }
 }
 
 function win() {
-    if (game.score === game.target.length) {
-        alphabet.onclick = '';
-        alert('You WIN!\nCongratulations.\nClick RESET to play again :-)');
-    }
+  if (game.score === game.target.length) {
+    alphabet.onclick = "";
+    alert("You WIN!\nCongratulations.\nClick RESET to play again :-)");
+  }
 }
 
 function gameOver() {
-    if (game.tries > 1) {
-        game.tries--;
-    } else {
-        alphabet.onclick = '';
-        alert('Game Over!\nThe word was: "' + game.target + '".\nClick RESET to play again :-)');
-    }
+  if (game.tries > 1) {
+    game.tries--;
+  } else {
+    alphabet.onclick = "";
+    alert(
+      'Game Over!\nThe word was: "' +
+        game.target +
+        '".\nClick RESET to play again :-)'
+    );
+  }
 }
 
 function guessUpdate(letter) {
-    for (index of game.indexes) {
-        document.getElementById("word-" + index).innerHTML = letter;
-    }
-    game.indexes = [];
+  for (index of game.indexes) {
+    document.getElementById("word-" + index).innerHTML = letter;
+  }
+  game.indexes = [];
 }
 
 function matchCount(letter) {
-    let pos = game.target.indexOf(letter);
-    while (pos != -1) {
-        game.indexes.push(pos);
-        pos = game.target.indexOf(letter, pos + 1);
-    }
-    game.score = game.score + game.indexes.length;
+  let pos = game.target.indexOf(letter);
+  while (pos != -1) {
+    game.indexes.push(pos);
+    pos = game.target.indexOf(letter, pos + 1);
+  }
+  game.score = game.score + game.indexes.length;
 }
 
 function randWord() {
-    return game.target = words[Math.floor(Math.random() * words.length)];
+  return (game.target = words[Math.floor(Math.random() * words.length)]);
 }
 
 function wordGen() {
-    for (let i = 0; i < game.target.length; i++) {
-        const newDiv = document.createElement('div');
-        newDiv.setAttribute('id', 'word-' + i);
-        word.appendChild(newDiv);
-    }
+  for (let i = 0; i < game.target.length; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "word-" + i);
+    word.appendChild(newDiv);
+  }
 }
 
 function alphabetGen() {
-    for (let i = 65; i <= 90; i++) {
-        const newButton = document.createElement('button');
-        const newContent = document.createTextNode(String.fromCharCode(i));
-        newButton.setAttribute('id', 'letter-' + i);
-        newButton.appendChild(newContent);
-        alphabet.appendChild(newButton);
-    }
+  for (let i = 65; i <= 90; i++) {
+    const newButton = document.createElement("button");
+    const newContent = document.createTextNode(String.fromCharCode(i));
+    newButton.setAttribute("id", "letter-" + i);
+    newButton.appendChild(newContent);
+    alphabet.appendChild(newButton);
+  }
 }
 
 function hangMan() {
-    canvas.style.display = "block";
-    randWord();
-    wordGen();
-    alphabetGen();
-    drawGallows();
-    console.log(game.target);
+  canvas.style.display = "block";
+  randWord();
+  wordGen();
+  alphabetGen();
+  drawGallows();
+  console.log(game.target);
 }
--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,29 +1,29 @@
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 const center = canvas.width / 2;
 
 function draw() {
-    switch (game.fails) {
-        case 1:
-            drawHead();
-            break;
-        case 2:
-            drawBody();
-            break;
-        case 3:
-            drawLeftLeg();
-            break;
-        case 4:
-            drawRightLeg();
-            break;
-        case 5:
-            drawLeftArm();
-            break;
-        case 6:
-            drawRightArm();
-            drawHead();
-            break;
-    }
+  switch (game.fails) {
+    case 1:
+      drawHead();
+      break;
+    case 2:
+      drawBody();
+      break;
+    case 3:
+      drawLeftLeg();
+      break;
+    case 4:
+      drawRightLeg();
+      break;
+    case 5:
+      drawLeftArm();
+      break;
+    case 6:
+      drawRightArm();
+      drawDead();
+      break;
+  }
 }
 
 // drawGallows();
@@ -37,116 +37,86 @@ function draw() {
 // drawDead();
 
 function drawGallows() {
-    ctx.strokeStyle = '#444';
-    ctx.lineWidth = 10;
-    ctx.beginPath();
-    ctx.moveTo(center, 75);
-    ctx.lineTo(center, 50);
-    ctx.lineTo(center - 100, 50);
-    ctx.lineTo(center - 100, 350);
-    ctx.lineTo(center + 100, 350);
-    ctx.stroke();
+  ctx.strokeStyle = "#444";
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(center, 75);
+  ctx.lineTo(center, 50);
+  ctx.lineTo(center - 100, 50);
+  ctx.lineTo(center - 100, 350);
+  ctx.lineTo(center + 100, 350);
+  ctx.stroke();
 }
 
 function drawHead() {
-    ctx.lineCap = 'round';
-    ctx.lineWidth = 5;
-    ctx.beginPath();
-    ctx.arc(center, 100, 25, 0, Math.PI * 2, true);
-    ctx.stroke();
+  ctx.lineCap = "round";
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  ctx.arc(center, 100, 25, 0, Math.PI * 2, true);
+  ctx.stroke();
 }
 
-// function drawSmile() {
-//     //Left eye
-//     ctx.lineWidth = 3;
-//     ctx.beginPath();
-//     ctx.arc(center - 8, 95, 5, 0, Math.PI * 2, true);
-//     ctx.stroke();
-//     //Right eye
-//     ctx.beginPath();
-//     ctx.arc(center + 8, 95, 5, 0, Math.PI * 2, true);
-//     ctx.stroke();
-//     //Mouth
-//     ctx.beginPath();
-//     ctx.arc(center, 108, 8, 0, Math.PI, false);
-//     ctx.stroke();
-// }
-
 function drawBody() {
-    ctx.lineWidth = 5;
-    ctx.beginPath();
-    ctx.moveTo(center, 125);
-    ctx.lineTo(center, 220);
-    ctx.stroke();
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  ctx.moveTo(center, 125);
+  ctx.lineTo(center, 220);
+  ctx.stroke();
 }
 
 function drawLeftLeg() {
-    ctx.beginPath();
-    ctx.moveTo(center, 220);
-    ctx.lineTo(center - 40, 300);
-    ctx.lineTo(center - 55, 290);
-    ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(center, 220);
+  ctx.lineTo(center - 40, 300);
+  ctx.lineTo(center - 55, 290);
+  ctx.stroke();
 }
 
 function drawRightLeg() {
-    ctx.beginPath();
-    ctx.moveTo(center, 220);
-    ctx.lineTo(center + 40, 300);
-    ctx.lineTo(center + 55, 290);
-    ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(center, 220);
+  ctx.lineTo(center + 40, 300);
+  ctx.lineTo(center + 55, 290);
+  ctx.stroke();
 }
 
 function drawLeftArm() {
-    ctx.beginPath();
-    ctx.moveTo(center, 140);
-    ctx.lineTo(center - 60, 180);
-    ctx.lineTo(center - 60, 190);
-    ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(center, 140);
+  ctx.lineTo(center - 60, 180);
+  ctx.lineTo(center - 60, 190);
+  ctx.stroke();
 }
 
 function drawRightArm() {
-    ctx.beginPath();
-    ctx.moveTo(center, 140);
-    ctx.lineTo(center + 60, 180);
-    ctx.lineTo(center + 60, 190);
-    ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(center, 140);
+  ctx.lineTo(center + 60, 180);
+  ctx.lineTo(center + 60, 190);
+  ctx.stroke();
 }
 
 function drawDead() {
-    //Left eye
-    ctx.lineWidth = 3;
-    ctx.beginPath();
-    ctx.moveTo(center - 11, 92);
-    ctx.lineTo(center - 5, 98);
-    ctx.moveTo(center - 11, 98);
-    ctx.lineTo(center - 5, 92);
-    ctx.stroke();
-    //Right eye
-    ctx.lineWidth = 3;
-    ctx.beginPath();
-    ctx.moveTo(center + 11, 92);
-    ctx.lineTo(center + 5, 98);
-    ctx.moveTo(center + 11, 98);
-    ctx.lineTo(center + 5, 92);
-    ctx.stroke();
-    //Mouth
-    ctx.lineWidth = 3;
-    ctx.beginPath();
-    ctx.moveTo(center - 5, 110);
-    ctx.lineTo(center + 5, 115);
-    ctx.stroke();
+  //Left eye
+  ctx.lineWidth = 3;
+  ctx.beginPath();
+  ctx.moveTo(center - 11, 92);
+  ctx.lineTo(center - 5, 98);
+  ctx.moveTo(center - 11, 98);
+  ctx.lineTo(center - 5, 92);
+  ctx.stroke();
+  //Right eye
+  ctx.lineWidth = 3;
+  ctx.beginPath();
+  ctx.moveTo(center + 11, 92);
+  ctx.lineTo(center + 5, 98);
+  ctx.moveTo(center + 11, 98);
+  ctx.lineTo(center + 5, 92);
+  ctx.stroke();
+  //Mouth
+  ctx.lineWidth = 3;
+  ctx.beginPath();
+  ctx.moveTo(center - 5, 110);
+  ctx.lineTo(center + 5, 115);
+  ctx.stroke();
 }
-
-// function drawWin() {
-//     ctx.font = '28px arial';
-//     ctx.textAlign = 'center';
-//     ctx.fillText('You WIN!', center, canvas.height - 70);
-//     ctx.fillText('Congratulations.', center, canvas.height - 40);
-// }
-
-// function drawGameOver() {
-//     ctx.font = '28px arial';
-//     ctx.textAlign = 'center';
-//     ctx.fillText('Game Over!', center, canvas.height - 70);
-//     ctx.fillText('The word was : "' + game.target + '"', center, canvas.height - 40);
-// }
