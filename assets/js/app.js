1f636d676272c743492da500425cae831a8f2eaa
epictete <john.laterre@gmail.com>
2020-08-11 16:32:12 +0200

    Added glow effects, code cleanup, final version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,20 @@
 // Constants
 const start = document.getElementById("start");
 const reset = document.getElementById("reset");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const center = canvas.width / 2;
 const word = document.getElementById("word");
 const alphabet = document.getElementById("alphabet");
 const text = document.getElementById("text");
+const glow = 300;
 const instructions =
   "Use your <i>keyboard</i> or <i>click</i> on the letters below.";
 
-//Variables
+//Game variables
 let game = {
   score: 0,
-  tries: 6,
+  tries: 5,
   fails: 0,
   currentLetter: "",
   usedLetters: [],
@@ -23,55 +27,17 @@ start.onclick = hangMan;
 reset.onclick = () => document.location.reload(true);
 
 //Functions
-function keyDownHandler() {
-  if (
-    event.keyCode > 64 &&
-    event.keyCode < 91 &&
-    !game.usedLetters.includes(event.key)
-  ) {
-    game.currentLetter = event.key.toLocaleLowerCase();
-    game.usedLetters.push(event.key.toLocaleLowerCase());
-    document.getElementById(game.currentLetter).disabled = true;
-    checkcurrentLetter();
-  }
-}
-
-function clickHandler() {
-  let temp = event.target.innerHTML.toLocaleLowerCase();
-  if (temp.length === 1) {
-    game.currentLetter = temp;
-    game.usedLetters.push(temp);
-    event.target.disabled = true;
-    checkcurrentLetter();
-  }
-}
-
-function checkcurrentLetter() {
-  if (game.target.includes(game.currentLetter)) {
-    document.getElementById(game.currentLetter).style.backgroundColor = "green";
-    matchCount();
-    guessUpdate();
-    win();
-  } else {
-    game.fails++;
-    document.getElementById(game.currentLetter).style.backgroundColor = "red";
-    draw();
-    gameOver();
-  }
-}
-
-function win() {
-  if (game.score === game.target.length) {
-    removeListeners();
-    alert("You WIN!\nCongratulations.\nClick RESET to play again :-)");
-  }
+function removeListeners() {
+  alphabet.onclick = "";
+  document.onkeydown = "";
 }
 
 function gameOver() {
-  if (game.tries > 1) {
+  if (game.tries > 0) {
     game.tries--;
   } else {
     removeListeners();
+    setTimeout(() => canvas.classList.add("red-glow"), glow + 10);
     alert(
       'Game Over!\nThe word was: "' +
         game.target +
@@ -80,14 +46,17 @@ function gameOver() {
   }
 }
 
-function removeListeners() {
-  alphabet.onclick = "";
-  document.onkeydown = "";
+function win() {
+  if (game.score === game.target.length) {
+    removeListeners();
+    setTimeout(() => canvas.classList.add("green-glow"), glow + 10);
+    alert("You WIN!\nCongratulations.\nClick RESET to play again :-)");
+  }
 }
 
 function guessUpdate() {
   for (index of game.indexes) {
-    document.getElementById("word-" + index).innerHTML = game.currentLetter;
+    document.getElementById(index).innerHTML = game.currentLetter;
   }
   game.indexes = [];
 }
@@ -101,20 +70,46 @@ function matchCount() {
   game.score = game.score + game.indexes.length;
 }
 
-function randWord() {
-  return (game.target = words[Math.floor(Math.random() * words.length)]);
+function checkCurrentLetter(currentLetter) {
+  game.currentLetter = currentLetter;
+  game.usedLetters.push(currentLetter);
+  if (game.target.includes(game.currentLetter)) {
+    document.getElementById(game.currentLetter).style.backgroundColor = "green";
+    canvas.classList.add("green-glow");
+    setTimeout(() => canvas.classList.remove("green-glow"), glow);
+    matchCount();
+    guessUpdate();
+    win();
+  } else {
+    document.getElementById(game.currentLetter).style.backgroundColor = "red";
+    canvas.classList.add("red-glow");
+    setTimeout(() => canvas.classList.remove("red-glow"), glow);
+    game.fails++;
+    draw();
+    gameOver();
+  }
 }
 
-function wordGen() {
-  for (let i = 0; i < game.target.length; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute("id", "word-" + i);
-    word.appendChild(newDiv);
+function clickHandler() {
+  let temp = event.target.innerHTML.toLocaleLowerCase();
+  if (temp.length === 1 && !game.usedLetters.includes(temp)) {
+    checkCurrentLetter(temp);
+  }
+}
+
+function keyDownHandler() {
+  let temp = event.key.toLocaleLowerCase();
+  if (
+    event.keyCode > 64 &&
+    event.keyCode < 91 &&
+    !game.usedLetters.includes(temp)
+  ) {
+    checkCurrentLetter(temp);
   }
 }
 
 function alphabetGen() {
-  for (let i = 65; i <= 90; i++) {
+  for (let i = 65; i < 91; i++) {
     const newButton = document.createElement("button");
     const newContent = document.createTextNode(String.fromCharCode(i));
     newButton.setAttribute("id", String.fromCharCode(i).toLocaleLowerCase());
@@ -123,11 +118,24 @@ function alphabetGen() {
   }
 }
 
+function wordGen() {
+  for (let i = 0; i < game.target.length; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", i);
+    word.appendChild(newDiv);
+  }
+}
+
+function randWord() {
+  return (game.target = words[Math.floor(Math.random() * words.length)]);
+}
+
 function hangMan() {
   start.disabled = true;
   start.blur();
   text.innerHTML = instructions;
   canvas.style.display = "block";
+  canvas.classList.add("gray-glow");
   randWord();
   wordGen();
   alphabetGen();
--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,7 +1,3 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const center = canvas.width / 2;
-
 function draw() {
   switch (game.fails) {
     case 1:
